0e23419ce38f713636f1a671736bdb35a7bc23d1
Thomas Barratt <tb@thomasbarratt.co.uk>
2019-09-23 10:19:11 +0100

    Extracted unused removeDuplicatedBy function

---
+++ src/components/functions/removeDuplicatesBy.js
@@ -0,0 +1,10 @@
+function removeDuplicatesBy(keyFn, array) {
+    var mySet = new Set();
+    return array.filter(function (x) {
+        var key = keyFn(x), isNew = !mySet.has(key);
+        if (isNew) mySet.add(key);
+        return isNew;
+    });
+}
+
+export default removeDuplicatesBy;
